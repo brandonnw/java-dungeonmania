d209f03485c4128d780ae7c9a368ea11c3637d06
Anthony Le <Anthony@student.unsw.edu.au>
2024-10-31 05:08:33 +1100

    Added print line

--- app/src/main/java/dungeonmania/entities/enemies/enemyMovement/CirclingMovement.java
+++ app/src/main/java/dungeonmania/entities/enemies/enemyMovement/CirclingMovement.java
@@ -2,29 +2,11 @@ package dungeonmania.entities.enemies.enemyMovement;
 
 import dungeonmania.Game;
 import dungeonmania.entities.enemies.Enemy;
-import dungeonmania.entities.enemies.Spider;
 
 public class CirclingMovement implements MovementStrategy {
     @Override
     public void move(Game game, Enemy enemy) {
-        if (!(enemy instanceof Spider)) {
-            return;
-        }
-
-        enemy = (Spider) enemy;
-        Position nextPos = movementTrajectory.get(nextPositionElement);
-        List<Entity> entities = game.getMap().getEntities(nextPos);
-        if (entities != null && entities.size() > 0 && entities.stream().anyMatch(e -> e instanceof Boulder)) {
-            forward = !forward;
-            updateNextPosition();
-            updateNextPosition();
-        }
-        nextPos = movementTrajectory.get(nextPositionElement);
-        entities = game.getMap().getEntities(nextPos);
-        if (entities == null || entities.size() == 0
-                || entities.stream().allMatch(e -> e.canMoveOnto(game.getMap(), this))) {
-            game.getMap().moveTo(this, nextPos);
-            updateNextPosition();
-        }
+        System.out.println("Spider goes in circles");
+        return;
     }
 }
