ffe3c2575355ac3644580234b5ec7f9c85a277df
Anthony Le <Anthony@student.unsw.edu.au>
2024-10-31 06:52:54 +1100

    Finished CirclingMovement strategy

--- app/src/main/java/dungeonmania/entities/enemies/enemyMovement/CirclingMovement.java
+++ app/src/main/java/dungeonmania/entities/enemies/enemyMovement/CirclingMovement.java
@@ -1,12 +1,46 @@
 package dungeonmania.entities.enemies.enemyMovement;
 
+import java.util.List;
+
 import dungeonmania.Game;
+import dungeonmania.entities.Boulder;
+import dungeonmania.entities.Entity;
 import dungeonmania.entities.enemies.Enemy;
+import dungeonmania.entities.enemies.Spider;
+import dungeonmania.util.Position;
 
 public class CirclingMovement implements MovementStrategy {
     @Override
     public void move(Game game, Enemy enemy) {
-        System.out.println("Spider goes in circles");
+        Spider spider = (Spider) enemy;
+        Position nextPos = spider.getNextPos();
+        List<Entity> entities = game.getMap().getEntities(nextPos);
+        if (entities != null && entities.size() > 0 && entities.stream().anyMatch(e -> e instanceof Boulder)) {
+            spider.setForward(!(spider.getForward()));
+            updateNextPosition(spider);
+            updateNextPosition(spider);
+        }
+        nextPos = spider.getNextPos();
+        entities = game.getMap().getEntities(nextPos);
+        if (entities == null || entities.size() == 0
+                || entities.stream().allMatch(e -> e.canMoveOnto(game.getMap(), spider))) {
+            game.getMap().moveTo(spider, nextPos);
+            updateNextPosition(spider);
+        }
         return;
+    }
+
+    private void updateNextPosition(Spider spider) {
+        if (spider.getForward()) {
+            spider.setNextPositionElement(spider.getNextPositionElement() + 1);
+            if (spider.getNextPositionElement() == 8) {
+                spider.setNextPositionElement(0);
+            }
+        } else {
+            spider.setNextPositionElement(spider.getNextPositionElement() - 1);
+            if (spider.getNextPositionElement() == -1) {
+                spider.setNextPositionElement(7);
+            }
+        }
     }
 }
