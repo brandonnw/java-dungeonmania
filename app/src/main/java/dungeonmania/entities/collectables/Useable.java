461638bb2dab9268731e94454bdcae607970b04f
Anthony <z5487558@ad.unsw.edu.au>
2024-11-02 20:13:44 +1100

    Changed Useable to abstract class

--- app/src/main/java/dungeonmania/entities/collectables/Useable.java
+++ app/src/main/java/dungeonmania/entities/collectables/Useable.java
@@ -1,8 +1,26 @@
 package dungeonmania.entities.collectables;
 
 import dungeonmania.Game;
+import dungeonmania.entities.inventory.InventoryItem;
+import dungeonmania.util.Position;
 
-public interface Useable {
-    public void use(Game game);
+public abstract class Useable extends InventoryItem {
+    private int durability;
+
+    public Useable(Position position, int durability) {
+        super(position);
+        this.durability = durability;
+    }
+
+    public void use(Game game) {
+        durability--;
+        if (durability <= 0) {
+            game.getPlayer().remove(this);
+        }
+    }
+
+    public int getDurability() {
+        return durability;
+    }
 
 }
